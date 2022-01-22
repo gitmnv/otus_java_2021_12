28ec363b2a509b7b831b50bcae2a3b939d55f9a4
Nikita Maltsev <nikita.maltsev@yandex.ru>
2022-01-22 04:29:42 +0300

    rename module hw-1

--- hw01-gradle/src/main/java/ru/otus/HelloOtus.java
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -13,7 +13,7 @@ import java.util.List;
  *
  * To start the application:
  * ./gradlew build
- * java -jar ./L01-gradle/build/libs/gradleHelloWorld-0.1.jar
+ * java -jar ./hw01-gradle/build/libs/gradleHelloWorld-0.1.jar
  *
  * To unzip the jar:
  * unzip -l L01-gradle.jar
