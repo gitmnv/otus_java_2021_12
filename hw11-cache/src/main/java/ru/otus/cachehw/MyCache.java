ba104e526a2349519b527780c196811718da222e
admen <nikita.maltsev@yandex.ru>
2022-04-17 23:37:58 +0300

    cache

--- hw11-cache/src/main/java/ru/otus/cachehw/MyCache.java
+++ hw11-cache/src/main/java/ru/otus/cachehw/MyCache.java
@@ -1,31 +1,53 @@
 package ru.otus.cachehw;
 
 
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.WeakHashMap;
+
 public class MyCache<K, V> implements HwCache<K, V> {
-//Надо реализовать эти методы
+    //Надо реализовать эти методы
+    List<WeakReference<HwListener<K, V>>> listeners = new ArrayList<>();
+    WeakHashMap<K, V> cache = new WeakHashMap<>();
 
     @Override
     public void put(K key, V value) {
-
+        cache.put(key, value);
+        notify(key,value,"put");
     }
 
     @Override
     public void remove(K key) {
-
+        notify(key, cache.remove(key),"remove");
     }
 
     @Override
     public V get(K key) {
-        return null;
+        V valuev = cache.get(key);
+        notify(key, valuev, "get");
+        return valuev;
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-
+        listeners.add(new WeakReference<>(listener));
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-
+        for (int i = 0; i< listeners.size(); i++){
+            var v = listeners.get(i).get();
+            if (v != null && v.equals(listener)){
+                listeners.remove(i);
+                return;
+            }
+        }
+    }
+    private void notify(K key, V value, String action){
+        for (WeakReference<HwListener<K, V>> listener : listeners) {
+            Objects.requireNonNull(listener.get()).notify(key, value, action);
+        }
     }
 }
