5db07e2c028b2511934e70c34d078d9c10ca2711
admen <nikita.maltsev@yandex.ru>
2022-04-29 01:19:45 +0300

    listeners fix

--- hw11-cache/src/main/java/ru/otus/cachehw/MyCache.java
+++ hw11-cache/src/main/java/ru/otus/cachehw/MyCache.java
@@ -1,19 +1,14 @@
 package ru.otus.cachehw;
 
-
 import java.lang.ref.WeakReference;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.WeakHashMap;
+import java.util.*;
 
 public class MyCache<K, V> implements HwCache<K, V> {
-    private final List<WeakReference<HwListener<K, V>>> listeners = new ArrayList<>();
+    private final List<HwListener<K, V>> listeners = new ArrayList<>();
     private final WeakHashMap<K, V> cache = new WeakHashMap<>();
 
     @Override
     public void put(K key, V value) {
-        System.out.println("cache!!!! " + cache);
         cache.put(key, value);
         notify(key, value, "put");
     }
@@ -25,31 +20,25 @@ public class MyCache<K, V> implements HwCache<K, V> {
 
     @Override
     public V get(K key) {
-        V valuev = cache.get(key);
-        notify(key, valuev, "get");
-        System.out.println(cache);
-        return valuev;
+        V value = cache.get(key);
+        notify(key, value, "get");
+        return value;
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-        listeners.add(new WeakReference<>(listener));
+        listeners.add(listener);
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-        for (int i = 0; i < listeners.size(); i++) {
-            var v = listeners.get(i).get();
-            if (v != null && v.equals(listener)) {
-                listeners.remove(i);
-                return;
-            }
-        }
+        listeners.remove(listener);
     }
 
     private void notify(K key, V value, String action) {
-        for (WeakReference<HwListener<K, V>> listener : listeners) {
-            Objects.requireNonNull(listener.get()).notify(key, value, action);
+
+        for (HwListener<K, V> listener : listeners) {
+            Objects.requireNonNull(listener).notify(key, value, action);
         }
     }
 }
