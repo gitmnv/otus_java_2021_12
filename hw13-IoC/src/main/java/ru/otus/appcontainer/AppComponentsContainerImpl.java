7cdc9e6551458cef2d249b05ee7a059b324d11a9
admen <nikita.maltsev@yandex.ru>
2022-04-30 19:28:03 +0300

    done

--- hw13-IoC/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ hw13-IoC/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,37 +1,78 @@
 package ru.otus.appcontainer;
 
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 
+import java.lang.reflect.Method;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     private final List<Object> appComponents = new ArrayList<>();
     private final Map<String, Object> appComponentsByName = new HashMap<>();
+    private final static Class<AppComponent> component = AppComponent.class;
+    private final static Class<AppComponentsContainerConfig> componentConfig = AppComponentsContainerConfig.class;
 
-    public AppComponentsContainerImpl(Class<?> initialConfigClass) {
+    public AppComponentsContainerImpl(Class<?> initialConfigClass) throws Exception {
         processConfig(initialConfigClass);
     }
 
-    private void processConfig(Class<?> configClass) {
+    private void processConfig(Class<?> configClass) throws Exception {
         checkConfigClass(configClass);
-        // You code here...
+
+        var instance = configClass.getConstructor().newInstance();
+        var methods = getComponentMethods(configClass);
+        var bean = new Object();
+
+        for (Method inspectedMethod : methods) {
+            if (inspectedMethod.getParameterCount() == 0) {
+                bean = inspectedMethod.invoke(instance);
+            } else {
+                bean = inspectedMethod.invoke(instance, getParams(inspectedMethod));
+            }
+            appComponents.add(bean);
+            appComponentsByName.put(inspectedMethod.getName(), bean);
+        }
+
+    }
+    private Object [] getParams(Method method){
+        return Arrays.stream(method.getParameterTypes()).map(
+                value -> Optional.ofNullable(getAppComponent(value))
+                        .orElseThrow(() -> new RuntimeException("method not found."))
+        ).toArray();
+    }
+
+    private List<Method> getComponentMethods(Class<?> configClass) {
+        return Arrays.stream(configClass.getMethods()).filter
+                (v -> v.isAnnotationPresent(component))
+                .sorted(Comparator.comparingInt(value ->
+                        value.getAnnotation(component).order()))
+                .collect(Collectors.toList());
     }
 
     private void checkConfigClass(Class<?> configClass) {
-        if (!configClass.isAnnotationPresent(AppComponentsContainerConfig.class)) {
+        if (!configClass.isAnnotationPresent(componentConfig)) {
             throw new IllegalArgumentException(String.format("Given class is not config %s", configClass.getName()));
         }
     }
 
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
+        for (Object appComponent : appComponents) {
+            if (appComponent.getClass().getName().equals(componentClass.getName())
+                    || Arrays.stream(appComponent.getClass().getInterfaces())
+                    .allMatch(v -> v.getName().equalsIgnoreCase(componentClass.getName()))
+            ) {
+                return (C) appComponent;
+            }
+        }
         return null;
     }
 
     @Override
     public <C> C getAppComponent(String componentName) {
-        return null;
+        return (C) appComponentsByName.get(componentName);
     }
 }
